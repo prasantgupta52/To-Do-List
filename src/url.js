769b23f4260a9e8e989602b3fda2c44b7958f499
PRASANT GUPTA <prasant123gupta@gmail.com>
2022-05-19 12:27:07 +0530

    Updates

--- src/url.js
+++
@@ -1,4 +0,0 @@
-const serverurl = "https://to-do-list-p.herokuapp.com" ;
-const connectionurl = "mongodb+srv://ToDoList:managedata@cluster0.81bae.mongodb.net/DataForToDo?retryWrites=true&w=majority";
-
-export {serverurl,connectionurl};
